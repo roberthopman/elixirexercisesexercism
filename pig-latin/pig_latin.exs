3f3c77ee5038e11ff2c730321aafdceb386c01a9
Robert Hopman <hopman.r@gmail.com>
2017-06-09 14:29:12 +0200

    Add improved pattern matching and regex

--- pig-latin/pig_latin.exs
+++ pig-latin/pig_latin.exs
@@ -19,56 +19,26 @@ defmodule PigLatin do
   end
 
   def match([]), do: []
-  def match([h|t]) do
+  def match([head|tail]) do
     cond do
-      String.slice(h, 0..2) == ("sch") ->
-        f = String.slice(h, 0..2)
-        m = String.slice(h, 3, String.length(h))
-        [m <> f <> "ay"]  ++ match(t)
-      String.slice(h, 0..2) == ("thr") ->
-        f = String.slice(h, 0..2)
-        m = String.slice(h, 3, String.length(h))
-        [m <> f <> "ay"]  ++ match(t)
-      String.slice(h, 0..2) == ("squ") ->
-        f = String.slice(h, 0..2)
-        m = String.slice(h, 3, String.length(h))
-        [m <> f <> "ay"]  ++ match(t)
+      Enum.member?(["sch", "thr", "squ"], (f = String.slice(head, 0..2))) ->
+        m = String.slice(head, 3, String.length(head))
+        [m <> f <> "ay"] ++ match(tail)
 
-      String.slice(h, 0..1) == ("qu") ->
-        f = String.slice(h, 0..1)
-        m = String.slice(h, 2, String.length(h))
-        [m <> f <> "ay"] ++ match(t)
-      String.slice(h, 0..1) == ("th") ->
-        f = String.slice(h, 0..1)
-        m = String.slice(h, 2, String.length(h))
-        [m <> f <> "ay"] ++ match(t)
-      String.slice(h, 0..1) == ("ch") ->
-        f = String.slice(h, 0..1)
-        m = String.slice(h, 2, String.length(h))
-        [m <> f <> "ay"] ++ match(t)
+      Enum.member?(["qu", "th", "ch"], (f = String.slice(head, 0..1))) ->
+        m = String.slice(head, 2, String.length(head))
+        [m <> f <> "ay"] ++ match(tail)
 
-      String.slice(h, 0..1) === ("xr") ->
-        [h <> "ay"]  ++ match(t)
-      String.slice(h, 0..1) === ("yt") ->
-        [h <> "ay"]  ++ match(t)
+      Enum.member?(["xr", "yt"], String.slice(head, 0..1)) ->
+        [head <> "ay"]  ++ match(tail)
 
+      Enum.member?(["a", "e", "i","o", "u"], String.slice(head, 0..0)) ->
+        [head <> "ay"]  ++ match(tail)
 
-
-      String.slice(h, 0..0) == ("a") ->
-        [h <> "ay"]  ++ match(t)
-      String.slice(h, 0..0) == ("e") ->
-        [h <> "ay"]  ++ match(t)
-      String.slice(h, 0..0) == ("i") ->
-        [h <> "ay"]  ++ match(t)
-      String.slice(h, 0..0) == ("o") ->
-        [h <> "ay"]  ++ match(t)
-      String.slice(h, 0..0) == ("u") ->
-        [h <> "ay"]  ++ match(t)
-
-      String.slice(h, 0..0) != ("consonants in regex") ->
-        f = String.slice(h, 0..0)
-        m = String.slice(h, 1, String.length(h))
-        [m <> f <> "ay"]  ++ match(t)
+      true ->
+        f = String.slice(head, 0..0)
+        m = String.slice(head, 1, String.length(head))
+        [m <> f <> "ay"]  ++ match(tail)
     end
   end
 end
@@ -77,3 +47,53 @@ end
 # 2 loop: recursion for each word OR replace with regex
 # 3 sliced word goes into pattern match
 # 4. pick apart the problem piece by piece
+
+# improved solution @xingzaizhailu
+
+    # # equals: @vowels ["a", "e", "i", "o", "u", "yt", "xr"]
+    # @vowels ~w(a e i o u yt xr)
+    # @consonants_2 ~w(ch qu th)
+    # @consonants_3 ~w(squ thr sch)
+
+    # @spec translate(phrase :: String.t()) :: String.t()
+    # def translate(phrase) do
+    #   phrase
+    #   |> String.split
+    #   |> Enum.map(&translate_one/1)
+    #   |> Enum.join(" ")
+    # end
+
+    # def translate_one(phrase) do
+    #   case String.starts_with?(phrase, @vowels) do
+    #     true ->
+    #       phrase <> "ay"
+    #     _ ->
+    #       head_size =
+    #         cond do
+    #           String.starts_with?(phrase, @consonants_3) -> 3
+    #           String.starts_with?(phrase, @consonants_2) -> 2
+    #           # if not a vowel or edge case, its a consonant:
+    #           true -> 1
+    #         end
+    #       #slice the last part of the string and concatenate with the first part
+    #       String.slice(phrase, head_size..-1) <> String.slice(phrase, 0..(head_size-1) ) <> "ay"
+    #   end
+    # end
+    # end
+
+# solution @buffalocoder
+
+  # def translate(phrase) do
+  #   phrase
+  #   |> String.split
+  #   |> Enum.map(&translate_word/1)
+  #   |> Enum.join(" ")
+  # end
+
+  # def translate_word(word) do
+  #   if Regex.match?(~r/\b([aeiou]|yt|xr).*/, word) do
+  #       word <> "ay"
+  #   else
+  #       Regex.replace(~r/\b(ch|qu|squ|thr|th|sch|.)(.*)/, word, "\\2\\1ay")
+  #   end
+  # end
