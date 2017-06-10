50d0920876617fa7134f5f7af263b34022058b99
Robert Hopman <hopman.r@gmail.com>
2017-06-10 15:36:03 +0200

    Add style improvement spage age

---
+++ bob/bob.exs
@@ -0,0 +1,7 @@
+defmodule Bob do
+  def hey(input) do
+    cond do
+
+    end
+  end
+end
--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -7,16 +7,18 @@ defmodule SpaceAge do
   aged on 'planet'.
   """
     @earth 31557600
+
     @planets %{
     earth: @earth,
-    neptune: 164.79132*@earth,
-    uranus: 84.016846*@earth,
-    saturn: 29.447498*@earth,
-    jupiter: 11.862615*@earth,
-    mars: 1.8808158*@earth,
-    venus: 0.61519726*@earth,
-    mercury: 0.2408467*@earth
+    neptune: 164.79132 * @earth,
+    uranus: 84.016846 * @earth,
+    saturn: 29.447498 * @earth,
+    jupiter: 11.862615 * @earth,
+    mars: 1.8808158 * @earth,
+    venus: 0.61519726 * @earth,
+    mercury: 0.2408467 * @earth
   }
+
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
     seconds / @planets[planet]
