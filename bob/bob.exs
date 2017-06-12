39b96cecf0304705bc347fde5abbf033c830dc29
Robert Hopman <hopman.r@gmail.com>
2017-06-12 11:55:03 +0200

    Add regex string processing

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,7 +1,15 @@
 defmodule Bob do
   def hey(input) do
     cond do
-
+        String.ends_with? input, "?" -> "Sure."
+        !String.match?(input, ~r/[[:lower:]]/) &&
+        String.match?(input, ~r/[[:alpha:]]/) -> "Whoa, chill out!"
+        String.trim(input) == "" -> "Fine. Be that way!"
+        true -> "Whatever."
     end
   end
 end
+
+# 1 Check structure of the problem
+# 2 Group & Order tests. ?, YELL, empty, whatever
+# 3 Solve
--- bob/bob_test.exs
+++ bob/bob_test.exs
@@ -7,7 +7,7 @@ ExUnit.configure exclude: :pending, trace: true
 
 defmodule BobTest do
   use ExUnit.Case
-  @tag :pending
+  # @tag :pending
   test "stating something" do
     assert Bob.hey("Tom-ay-to, tom-aaaah-to.") == "Whatever."
   end
@@ -17,7 +17,7 @@ defmodule BobTest do
     assert Bob.hey("Does this cryogenic chamber make me look fat?") == "Sure."
   end
 
-  @tag :pending
+  # @tag :pending
   test "talking forcefully" do
     assert Bob.hey("Let's go make out behind the gym!") == "Whatever."
   end
@@ -52,32 +52,32 @@ defmodule BobTest do
     assert Bob.hey("УХОДИ") == "Whoa, chill out!"
   end
 
-  @tag :pending
+  # @tag :pending
   test "shouting with no exclamation mark" do
     assert Bob.hey("I HATE YOU") == "Whoa, chill out!"
   end
 
-  @tag :pending
+  # @tag :pending
   test "statement containing question mark" do
     assert Bob.hey("Ending with ? means a question.") == "Whatever."
   end
 
-  @tag :pending
+  # @tag :pending
   test "silence" do
     assert Bob.hey("") == "Fine. Be that way!"
   end
 
-  @tag :pending
+  # @tag :pending
   test "prolonged silence" do
     assert Bob.hey("  ") == "Fine. Be that way!"
   end
 
-  @tag :pending
+  # @tag :pending
   test "only numbers" do
     assert Bob.hey("1, 2, 3") == "Whatever."
   end
 
-  @tag :pending
+  # @tag :pending
   test "question with numbers" do
     assert Bob.hey("4?") == "Sure."
   end
