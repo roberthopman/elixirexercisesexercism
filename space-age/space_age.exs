5afb870fa226997e0fabe9caf5d9d4f5e8ad0a60
Robert Hopman <hopman.r@gmail.com>
2017-06-12 11:57:58 +0200

    Add indentation of Spage Age

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -6,18 +6,18 @@ defmodule SpaceAge do
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
-    @earth 31557600
+  @earth 31557600
 
-    @planets %{
-    earth: @earth,
-    neptune: 164.79132 * @earth,
-    uranus: 84.016846 * @earth,
-    saturn: 29.447498 * @earth,
-    jupiter: 11.862615 * @earth,
-    mars: 1.8808158 * @earth,
-    venus: 0.61519726 * @earth,
-    mercury: 0.2408467 * @earth
-  }
+  @planets %{
+  earth: @earth,
+  neptune: 164.79132 * @earth,
+  uranus: 84.016846 * @earth,
+  saturn: 29.447498 * @earth,
+  jupiter: 11.862615 * @earth,
+  mars: 1.8808158 * @earth,
+  venus: 0.61519726 * @earth,
+  mercury: 0.2408467 * @earth
+}
 
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
