3c1499165dc06e854710c5d1425c847aadfcf9ae
Robert Hopman <hopman.r@gmail.com>
2017-06-27 16:59:50 +0200

    Add group_by, map, into

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,6 +6,20 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
+    sentence
+    |> String.replace(~r/[:!&@$%^,]/, "")
+    |> String.replace(~r/_/, " ")
+    |> String.downcase()
 
+    # v1
+    |> String.split()
+    |> Enum.group_by(fn x -> x end)
+    |> Enum.map(fn {k, v} -> {k, Enum.count(v)} end)
+    |> Enum.into(%{})
+
+    # from https://stackoverflow.com/a/37463717/6430382
+    # v2
+    # String.split()
+    # |> Enum.reduce(%{}, fn word, acc -> Map.update(acc, word, 1, &(&1 + 1)) end)
   end
 end
