2c93b25a6e86d2667ee03ec47c8cf7e9915751b0
Robert Hopman <hopman.r@gmail.com>
2017-07-11 13:23:53 +0200

    Compare two lists

--- sublist/sublist.exs
+++ sublist/sublist.exs
@@ -4,6 +4,30 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
-
+    cond do
+      a === b        -> :equal
+      sublist?(a, b) -> :sublist
+      sublist?(b, a) -> :superlist
+      true           -> :unequal
+    end
   end
+
+  defp sublist?(a, y) when length(a) <= length(y) do
+    cond do
+      a === Enum.take(y, length(a)) -> true
+      true -> sublist?(a, tl(y))
+    end
+  end
+  defp sublist?(_, _), do: false
 end
+
+# start comparing with cond do
+# equal is easy: a===b
+# then start pattern matching
+# for sublist, match a with take(y, length(a))
+# otherwise do recursively (a, tl(y))
+# if nothing matches, give false
+# same for superlist but (a,b) is (b,a)
+# add guard clause to guard for edge cases
+#
+# from: https://github.com/baschtl/elixir.challenges/blob/master/sublist/sublist.exs
